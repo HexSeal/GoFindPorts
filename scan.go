3a56482366f4bd9e35e4e7837a50699a67e9b76d
HexSeal <maxfin13@gmail.com>
2020-10-09 20:59:55 -0400

    [add] Scanning multiple ports, but one by one.

--- scan.go
+++ scan.go
@@ -6,11 +6,22 @@ import (
 )
 
 func main() {
-	// First query is type, second is where to scan(testing site)
-	_, err := net.Dial("tcp", "scanmen.nmap.org:80")
-	
-	// If there is no error, print telling the user the connection was successful
-	if err == nil {
-		fmt.Println("Connection successful")
+	// For testing, scan the first 1024 ports
+	for i := 1; i <= 1024; i++ {
+		// Get this iteration's address
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		
+		// Establish the connection
+		conn, err := net.Dial("tcp", address)
+
+		// Handle errors
+		if err != nil {
+			// If we can't access the port, because it's either closed or filtered
+			continue
+		}
+
+		// Close the connection once it's done to be good internet neighbors
+		conn.Close()
+		fmt.Printf("%d is open\n", i)
 	}
 }
