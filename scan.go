db72eee7c780b56cb427dbe1e8beca4f35658423
HexSeal <maxfin13@gmail.com>
2020-10-09 21:10:03 -0400

    [add] Concurrency but it's too fast to establish connections

--- scan.go
+++ scan.go
@@ -8,20 +8,23 @@ import (
 func main() {
 	// For testing, scan the first 1024 ports
 	for i := 1; i <= 1024; i++ {
-		// Get this iteration's address
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
-		
-		// Establish the connection
-		conn, err := net.Dial("tcp", address)
+		// Want to do it faster? Wrap it in a go routine.
+		go func(j int) {
+			// Get this iteration's address
+			address := fmt.Sprintf("scanme.nmap.org:%d", j)
+			
+			// Establish the connection
+			conn, err := net.Dial("tcp", address)
 
-		// Handle errors
-		if err != nil {
-			// If we can't access the port, because it's either closed or filtered
-			continue
-		}
+			// Handle errors
+			if err != nil {
+				// If we can't access the port, because it's either closed or filtered
+				return
+			}
 
-		// Close the connection once it's done to be good internet neighbors
-		conn.Close()
-		fmt.Printf("%d is open\n", i)
+			// Close the connection once it's done to be good internet neighbors
+			conn.Close()
+			fmt.Printf("%d is open\n", j)
+		}(i) 
 	}
 }
